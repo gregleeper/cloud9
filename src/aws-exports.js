cce791d19fdc07d74e9bd551bd9fe113d252c390
Greg Leeper <gregleeper@Gregs-MacBook-Pro.local>
2020-09-15 19:53:08 -0500

    auth update

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -16,8 +16,8 @@ const awsmobile = {
             "profile",
             "aws.cognito.signin.user.admin"
         ],
-        "redirectSignIn": "http://localhost:3000/",
-        "redirectSignOut": "http://localhost:3000/",
+        "redirectSignIn": "http://localhost:3000/,https://cloud9-nu.vercel.app/",
+        "redirectSignOut": "http://localhost:3000/,https://cloud9-nu.vercel.app/",
         "responseType": "code"
     },
     "federationTarget": "COGNITO_USER_POOLS",
